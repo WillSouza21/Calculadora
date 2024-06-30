fc14a7bf90bb6886482a0c314a934236a67dfeaf
Toshino16 <hermanoguilherme4@gmail.com>
2024-06-30 10:09:43 -0300

    Adcicionar a subtração no códdigo

--- scripts/calculator.py
+++ scripts/calculator.py
@@ -1,6 +1,7 @@
 print("O que deseja fazer?")
 print("0.Sair")
 print("1.Somar")
+print("2.Subtrair")
 option = int(input())
 
 if option == 0:
@@ -9,4 +10,7 @@ elif option == 1:
     number1 = int(input('Digite o Primeiro Número: '))
     number2 = int(input('Digite o Segundo Número: '))
     print('Resposta dessa soma é: ', number1 + number2)
-    
+elif option == 2:
+    number1 = int(input('Digite o Primeiro Número: '))
+    number2 = int(input('Digite o Segundo Número: '))
+    print('Resposta dessa subtração é: ', number1 - number2)
