1e8a01d41fc15e15f3c690e142e20164c5290ccb
Toshino16 <hermanoguilherme4@gmail.com>
2024-06-30 10:03:49 -0300

    Adicionar a soma no código

--- scripts/calculator.py
+++ scripts/calculator.py
@@ -1,6 +1,12 @@
 print("O que deseja fazer?")
 print("0.Sair")
+print("1.Somar")
 option = int(input())
 
 if option == 0:
     exit()
+elif option == 1:
+    number1 = int(input('Digite o Primeiro Número: '))
+    number2 = int(input('Digite o Segundo Número: '))
+    print('Resposta dessa soma é: ', number1 + number2)
+    
