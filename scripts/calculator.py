ecfa1c87da124dad51f1e69469dab1fdc511db9e
Will <williamcavalcante912@gmail.com>
2024-07-01 19:30:44 -0300

    Resolver bug na divisao por zero fix #1

--- scripts/calculator.py
+++ scripts/calculator.py
@@ -4,23 +4,35 @@ print("1.Somar")
 print("2.Subtrair")
 print("3.Multiplicar")
 print("4.Dividir")
-option = int(input())
+option = int(input()) # definindo variável option do tipo inteiro
 
+#Fechar o código após digitar zero
 if option == 0:
     exit()
+
+# Soma
 elif option == 1:
     number1 = int(input('Digite o Primeiro Número: '))
     number2 = int(input('Digite o Segundo Número: '))
-    print('Resposta dessa soma é: ', number1 + number2)
+    print('Resposta dessa soma é: ', number1 + number2) 
+
+# Subtração
 elif option == 2:
     number1 = int(input('Digite o Primeiro Número: '))
     number2 = int(input('Digite o Segundo Número: '))
-    print('Resposta dessa subtração é: ', number1 - number2)
+    print('Resposta dessa subtração é: ', number1 - number2) 
+
+# Multiplicação
 elif option == 3:
     number1 = int(input('Digite o Primeiro Número: '))
     number2 = int(input('Digite o Segundo Número: '))
-    print('Resposta dessa multiplicação é: ', number1 * number2)
+    print('Resposta dessa multiplicação é: ', number1 * number2) 
+
+# Divisão
 elif option == 4:
     number1 = int(input('Digite o Primeiro Número: '))
     number2 = int(input('Digite o Segundo Número: '))
-    print('Resposta dessa divisão é: ', number1/number2)
+    if number2 == 0:  ## If para impedir a quebra do código com divisões por zero
+        print('Não é possível realizar divisão por zero.')
+        exit()
+    print('Resposta dessa divisão é: ', number1 / number2) 
