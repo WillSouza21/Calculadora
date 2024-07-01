a62bbe1356a43ef4db54c98219a882fa116e3d89
Toshino16 <hermanoguilherme4@gmail.com>
2024-07-01 19:40:26 -0300

    adicionar sistema de repeticao closed #2

--- scripts/calculator.py
+++ scripts/calculator.py
@@ -1,26 +1,27 @@
-print("O que deseja fazer?")
-print("0.Sair")
-print("1.Somar")
-print("2.Subtrair")
-print("3.Multiplicar")
-print("4.Dividir")
-option = int(input())
+while True:
+    print("O que deseja fazer?")
+    print("0.Sair")
+    print("1.Somar")
+    print("2.Subtrair")
+    print("3.Multiplicar")
+    print("4.Dividir")
+    option = int(input())
 
-if option == 0:
-    exit()
-elif option == 1:
-    number1 = int(input('Digite o Primeiro Número: '))
-    number2 = int(input('Digite o Segundo Número: '))
-    print('Resposta dessa soma é: ', number1 + number2)
-elif option == 2:
-    number1 = int(input('Digite o Primeiro Número: '))
-    number2 = int(input('Digite o Segundo Número: '))
-    print('Resposta dessa subtração é: ', number1 - number2)
-elif option == 3:
-    number1 = int(input('Digite o Primeiro Número: '))
-    number2 = int(input('Digite o Segundo Número: '))
-    print('Resposta dessa multiplicação é: ', number1 * number2)
-elif option == 4:
-    number1 = int(input('Digite o Primeiro Número: '))
-    number2 = int(input('Digite o Segundo Número: '))
-    print('Resposta dessa divisão é: ', number1/number2)
+    if option == 0:
+        exit()
+    elif option == 1:
+        number1 = int(input('Digite o Primeiro Número: '))
+        number2 = int(input('Digite o Segundo Número: '))
+        print('Resposta dessa soma é: ', number1 + number2)
+    elif option == 2:
+        number1 = int(input('Digite o Primeiro Número: '))
+        number2 = int(input('Digite o Segundo Número: '))
+        print('Resposta dessa subtração é: ', number1 - number2)
+    elif option == 3:
+        number1 = int(input('Digite o Primeiro Número: '))
+        number2 = int(input('Digite o Segundo Número: '))
+        print('Resposta dessa multiplicação é: ', number1 * number2)
+    elif option == 4:
+        number1 = int(input('Digite o Primeiro Número: '))
+        number2 = int(input('Digite o Segundo Número: '))
+        print('Resposta dessa divisão é: ', number1/number2)
