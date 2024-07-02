4e10bc7d31c9271c50367f261299c691b16bf418
Toshino16 <hermanoguilherme4@gmail.com>
2024-07-02 19:21:52 -0300

    Evitar o bug de inserir caractere closed #4

--- scripts/calculator.py
+++ scripts/calculator.py
@@ -1,3 +1,10 @@
+def obter_numero(texto=''):
+    while True:
+        resposta = input(texto)
+        if resposta.isdigit():
+            return int(resposta)
+        else:
+            print("Isso não é um numero.")
 while True:
     print("O que deseja fazer?")
     print("0.Sair")
@@ -5,7 +12,7 @@ while True:
     print("2.Subtrair")
     print("3.Multiplicar")
     print("4.Dividir")
-    option = int(input())
+    option = obter_numero()
 
 #Fechar o código após digitar zero
     if option == 0:
@@ -13,27 +20,27 @@ while True:
 
 # Soma
     elif option == 1:
-        number1 = int(input('Digite o Primeiro Número: '))
-        number2 = int(input('Digite o Segundo Número: '))
+        number1 = obter_numero('Digite o Primeiro Número: ')
+        number2 = obter_numero('Digite o Segundo Número: ')
         print('Resposta dessa soma é: ', number1 + number2) 
 
 # Subtração
     elif option == 2:
-        number1 = int(input('Digite o Primeiro Número: '))
-        number2 = int(input('Digite o Segundo Número: '))
+        number1 = obter_numero('Digite o Primeiro Número: ')
+        number2 = obter_numero('Digite o Segundo Número: ')
         print('Resposta dessa subtração é: ', number1 - number2) 
 
 # Multiplicação
     elif option == 3:
-        number1 = int(input('Digite o Primeiro Número: '))
-        number2 = int(input('Digite o Segundo Número: '))
+        number1 = obter_numero('Digite o Primeiro Número: ')
+        number2 = obter_numero('Digite o Segundo Número: ')
         print('Resposta dessa multiplicação é: ', number1 * number2) 
 
 # Divisão
     elif option == 4:
-        number1 = int(input('Digite o Primeiro Número: '))
-        number2 = int(input('Digite o Segundo Número: '))
-        if number2 == 0:  ## If para impedir a quebra do código com divisões por zero
+        number1 = obter_numero('Digite o Primeiro Número: ')
+        number2 = obter_numero('Digite o Segundo Número: ')
+        if number2 == 0:  ## I2f para impedir a quebra do código com divisões por zero
             print('Não é possível realizar divisão por zero.')
             continue
         print('Resposta dessa divisão é: ', number1 / number2) 
